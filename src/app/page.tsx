ce2f6004bb11617ed147d81443540bafc571ad83
Johnny Talbot <johnnywtalbot@gmail.com>
2025-05-31 10:01:31 +0800

    sourcing fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@
 
 import React, { useEffect, useState, useRef } from 'react';
 
-import Navigation from "@/components/Navigation";
-import Background from "@/components/Background";
-import About from "@/components/About";
-import Experience from '@/components/Experience';
-import Projects from '@/components/Projects';
-import Contact from '@/components/Contact';
+import Navigation from "../components/Navigation";
+import Background from "../components/Background";
+import About from "../components/About";
+import Experience from '../components/Experience';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
 
 export default function Home() {
   const [showNav, setShowNav] = useState(true);
